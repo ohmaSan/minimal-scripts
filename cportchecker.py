a298d71808f927ea1dfa1c3a4a3ca51c63a57ea0
ohma <61019105+ohmaSan@users.noreply.github.com>
2020-05-25 21:50:44 +0300

    Create cportchecker.py

---
+++ cportchecker.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+# Tokuchi Toua
+# cportchecker.py (Python3 Spesific Port Checker)
+
+from scapy.layers.inet import TCP, IP
+from scapy.all import *
+import time
+
+print("""
+##############################
+### CSyn Port Checker v1.1 ###
+##############################
+1.Default Settings
+\ta.One SYN Packet Send Every Scan
+If you want to exit press  Ctrl+C
+If you don't want see verbose, change the verbose value with False
+""")
+time.sleep(1)
+
+if len(sys.argv) != 4:
+    print("usege: ./cportchecker.py <TargetIP> <Target Port> <Source Port>\n")
+    sys.exit()
+
+targe = sys.argv[1]
+d_port = int(sys.argv[2])
+s_port = int(sys.argv[3])
+
+print("Checking is started..!")
+time.sleep(0.6)
+
+def portchecker():
+    ip = IP(dst=targe)
+    tcp = TCP(sport = s_port,dport=d_port, flags='S')
+    ressponse = sr1((ip/tcp),timeout=3)
+    flag_type = ressponse[TCP].flags
+    if flag_type == 'SA':
+        print(f"{d_port} is open!")
+    elif flag_type == 'RA':
+        print(f"{d_port} is open!")
+    else:
+        print(f"{d_port} is filtered or close!!")
+
+
+portchecker()
