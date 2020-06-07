78e36a96571bbdacb010ac811ce5375e37895559
ohma <61019105+ohmaSan@users.noreply.github.com>
2020-06-07 16:27:12 +0300

    Update wordcrawler.py

--- wordcrawler.py
+++ wordcrawler.py
@@ -1,9 +1,8 @@
-# Web site word crawler
-
 import requests
 from bs4 import BeautifulSoup
 
 target = input("Target: ")
+print("#" * 50)
 
 
 def crawler():
@@ -11,7 +10,8 @@ def crawler():
     dirty = BeautifulSoup(respons.content, "lxml")
     clean_list = set(dirty.text.split())
     for clean in clean_list:
-        print(clean)
+        print(clean.strip(',').strip('.').strip('"').strip(':').strip('(').strip(')').strip(':'))
+    print(f"\nNumber of Words: {len(clean_list)}")
 
 
 crawler()
