3524b16b9c7dff97ffe51181466b78d22831b917
ohma <61019105+ohmaSan@users.noreply.github.com>
2021-03-08 01:20:27 +0300

    Update bscscanwalletstalker.py

--- bscscanwalletstalker.py
+++ bscscanwalletstalker.py
@@ -5,21 +5,22 @@ print("bscscanWalletStalker v1.1")
 
 bscscan = "https://api.bscscan.com"
 token = "0xf859Bf77cBe8699013d6Dbc7C2b926Aaf307F830"
-api_key = "YOUR API KEY"
+api_key = "5YFEM1PDQW6QA5YY9FK9J9Z9WEP671SB95"
 
 key = 0
 print("First time:" + time.strftime('%X %x'))
 
-with open("whales.txt") as f:
-    print("Program is running! stalking.txt is creating..!\n")
+while True:
+    with open("whales.txt") as f:
+        print("Program is running! stalking.txt is creating..!\n")
 
-    for wallets in f:
-        r = requests.get(
-            f'{bscscan}/api?module=account&action=tokenbalance&contractaddress={token}&address={wallets.strip()}&tag=latest&apikey={api_key}')
-        print(time.strftime('%X %x ==> ') + wallets.strip() + " = " + r.text.strip())
-        with open("stalking.txt", "a") as f:
-            f.write(time.strftime('%X %x ==> ') + wallets.strip() + " = " + r.text.strip() + "\n")
-        key += 1
-        if key == 5:
-            time.sleep(1)
-            key = 0
+        for wallets in f:
+            r = requests.get(
+                f'{bscscan}/api?module=account&action=tokenbalance&contractaddress={token}&address={wallets.strip()}&tag=latest&apikey={api_key}')
+            print(time.strftime('%X %x ==> ') + wallets.strip() + " = " + r.text.strip())
+            with open("stalking.txt", "a") as f:
+                f.write(time.strftime('%X %x ==> ') + wallets.strip() + " = " + r.text.strip() + "\n")
+            key += 1
+            if key == 5:
+                time.sleep(1)
+                key = 0
