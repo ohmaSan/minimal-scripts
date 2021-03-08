25b7c6601c1c495b61c7b1a42b6ab567e155ae0a
ohma <61019105+ohmaSan@users.noreply.github.com>
2021-03-08 05:37:38 +0300

    Create whaleanalysis.py

---
+++ whaleanalysis.py
@@ -0,0 +1,26 @@
+#whale analyzer, just 1 whale
+
+with open("stalking.txt") as f:
+        firstline = f.readlines()[1:2]
+        wallet = firstline[0][24:64]
+        fbalance = firstline[0][106:131]
+        firstBalance = "".join(fbalance[0:7])
+        intBalance = int(firstBalance)
+
+
+with open("stalking.txt") as f:
+        for line in f:
+            pass
+        last_line = line
+        lbalance = last_line[106:131]
+        lastBalance = "".join(lbalance[0:7])
+
+sell = int(firstBalance) - int(lastBalance)
+buy = int(lastBalance) - int(lastBalance)
+
+if firstBalance > lastBalance:
+    print(f"While buy count: {sell}")
+else:
+    print(f"While sell count: {buy}" )
+
+
