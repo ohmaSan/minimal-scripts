26f35bb8e05a7067e0884634723939e359a53da3
ohma <61019105+ohmaSan@users.noreply.github.com>
2021-03-08 05:57:33 +0300

    Update whaleanalysis.py

--- whaleanalysis.py
+++ whaleanalysis.py
@@ -24,3 +24,39 @@ else:
     print(f"Whale sell count: {sell}" )
 
 
+"""
+import time
+#whale analyzer
+
+
+with open("stalking.txt") as f:
+        firstline = f.readlines()[1:2]
+        wallet = firstline[0][24:64]
+        fbalance = firstline[0][106:131]
+        firstBalance = "".join(fbalance[0:7])
+        intBalance = int(firstBalance)
+
+
+with open("stalking.txt") as f:
+        for line in f:
+            pass
+        last_line = line
+        lbalance = last_line[106:131]
+        lastBalance = "".join(lbalance[0:7])
+
+sell = int(firstBalance) - int(lastBalance)
+buy = int(lastBalance) - int(lastBalance)
+
+if firstBalance > lastBalance:
+    print(f"While buy count: {sell}")
+    with open("/home/x/Desktop/berry/hunt/signalling.txt","a") as f:
+        f.write(f"While buy coint: {sell}")
+else:
+    print(f"While sell count: {buy}" )
+    with open("/home/x/Desktop/berry/hunt/signalling.txt","a") as f:
+        f.write(f"While sell cout: {buy}")
+
+
+
+
+"""
